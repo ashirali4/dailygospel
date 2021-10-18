758228c974f85954b0d793f30422b64c0cc1af59
ashir.muhammad <ALIali123!>
2021-10-18 06:33:42 -0700

    Firebase Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:dailygospel/src/screens/main_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Daily Gospel',
+      title: 'Biblx',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
